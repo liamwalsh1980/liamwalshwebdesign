921ec6743abc9f0b7179d219d1c0bb958bd52644
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-09-08 20:49:48 +0000

    Added About Page view

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 @app.route("/services")
 def services():
     return render_template("services.html")
