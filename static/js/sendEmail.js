aa947b2a183a6091a8750d0e15a356744211af5f
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-12-27 13:24:30 +0000

    Wired up contact form with emailjs

---
+++ static/js/sendEmail.js
@@ -0,0 +1,32 @@
+function sendMail(contactForm) {
+    var modal = document.getElementById("modal");
+
+    emailjs.send("service_bv0x1rb", "purecode", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "contact_number": contactForm.number.value,
+        "more_information": contactForm.message.value,
+        "new_website": contactForm.option_1.value,
+        "re-design": contactForm.option_2.value,
+        "domain_and_hosting": contactForm.option_.value,
+        "other": contactForm.option_4.value
+    })
+    .then(
+        function (response) {
+            console.log("SUCCESS", response);
+            $(".modal-message").text("Thank you for project request " + contactForm.name.value + ". You will receive an email or call very soon to dicuss further");
+            modal.style.display = "block";
+            $("#closing-btn").click(function () {
+                location.reload();
+            });
+        },
+        function (error) {
+            console.log("FAILED", error);
+            $(".modal-message").text("Sorry " + contactForm.name.value + " something went wrong. Please try submitting your request again!");
+            modal.style.display = "block";
+            $("#closing-btn").click(function () {
+                location.reload();
+            });
+        });
+    return false;
+}
