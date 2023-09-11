287102c29285b1b2151a9790f7f4813571c7156d
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2023-09-11 21:13:12 +0000

    Updated text

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def portfolio():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
