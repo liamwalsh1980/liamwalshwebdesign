14c82825132eed5ff16d4229caa893d8a4cf8d75
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2023-12-31 15:17:43 +0000

    Removed unused page

--- app.py
+++ app.py
@@ -24,12 +24,7 @@ def contact():
 @app.route("/about")
 def about():
     return render_template("about.html")
-
-
-@app.route("/services")
-def services():
-    return render_template("services.html")
-
+    
 
 @app.route("/portfolio")
 def portfolio():
